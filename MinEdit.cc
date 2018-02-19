5fa9374f9b444a9c82585fa03fab74f3e8d1df21
Abdelghafour Mourchid <Abdelghafour01@users.noreply.github.com>
2018-02-19 23:04:14 +0000

    Update MinEdit.cc

--- MinEdit.cc
+++ MinEdit.cc
@@ -7,24 +7,33 @@
 using namespace std;
 
 int  minEdit(string s, string t, int n, int m) {
-  int d1, d2, d3, cost;
-  int i, j;
-  int d[n + 1][m + 1];
-  for(i = 0;i <= n;i++) {
-      for(j = 0;j <= m;j++) {
-      if(s[i - 1]==t[j - 1]) cost = 0;
-      else cost=1;
+  int cost;
+  int d[n + 1][m + 1] = {{0}};
 
-      d1 = d[i][j + 1] + 1;
-      d2 = d[i + 1][j] + 1;
-      d3 = d[i][j] + cost;
-      d[i + 1][j + 1] = min(min(d1, d2), d3);
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= m; j++) {
+          if(s[i - 1] == t[j - 1]) cost = 0;
+          else cost = 1;
 
-      if(i > 0 && j > 0 && s[i + 1] == t[j] && s[i] == t[j + 1] ) {
-        d[i + 1][j + 1] = min(d[i + 1][j + 1],d[i - 1][j - 1] + cost);
-      }
-   }
+          int d1 = d[i][j + 1] + 1;
+          int d2 = d[i + 1][j] + 1;
+          int d3 = d[i][j] + cost;
+
+          d[i + 1][j + 1] = min(min(d1, d2), d3);
+
+          if(i > 0 && j > 0 && s[i + 1] == t[j] && s[i] == t[j + 1] ) {
+            d[i + 1][j + 1] = min(d[i + 1][j + 1],d[i - 1][j - 1] + cost);
+          }
+        }
+    }
+
+    for(int i = 1; i <= n + 1; i++){
+        for(int j = 1; j <= m + 1; j++){
+            cout << d[i][j] << " ";
+        }
+    cout << endl;
  }
+
  return d[n+1][m+1];
 }
 
